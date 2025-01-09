e43154c652737dfd59eefa331e127f144601096d
Sneha_Vijapur <148443328+Sneha-c-v@users.noreply.github.com>
2025-01-09 10:45:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,7 +4,9 @@ terraform {
   storage_account_name = "tfstorage1705"
   container_name       = "tfstate"
   key                  = "terraform.tfstate"
-  tenant_id            = "139f0669-cebb-42df-b9e3-415009ddb690"
-  subscription_id      = "cfe55393-4b39-47ea-95d2-079eae01dd1e"
+  tenant_id            = var.tenant_id
+  subscription_id      = var.subscription_id
+  client_id            = var.client_id
+  client_secret        = var.client_secret
   }
 }
