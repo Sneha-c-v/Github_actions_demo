b1ed84de284ffc8c211143e1fefc64bc48f2242f
Sneha_Vijapur <148443328+Sneha-c-v@users.noreply.github.com>
2025-01-09 10:57:29 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,9 +4,9 @@ terraform {
   storage_account_name = "tfstorage1705"
   container_name       = "tfstate"
   key                  = "terraform.tfstate"
-  tenant_id            = var.tenant_id
-  subscription_id      = var.subscription_id
-  client_id            = var.client_id
-  client_secret        = var.client_secret
+  tenant_id            = env("ARM_TENANT_ID")
+  subscription_id      = env("ARM_SUBSCRIPTION_ID")
+  client_id            = env("ARM_CLIENT_ID")
+  client_secret        = env("ARM_CLIENT_SECRET")
   }
 }
