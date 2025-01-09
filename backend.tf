23a9c653a09b1fead5e7fb3e70c2ae70e7406d06
Sneha Vijapur <snehavijapur1705@gmail.com>
2025-01-09 11:17:53 +0530

    formatted the tf files

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 terraform {
   backend "azurerm" {
-  resource_group_name  = "actions"
-  storage_account_name = "tfstate1705"
-  container_name       = "tfstate"
-  key                  = "terraform.tfstate"
- # tenant_id            = env("ARM_TENANT_ID")
- #subscription_id      = env("ARM_SUBSCRIPTION_ID")
- # client_id            = env("ARM_CLIENT_ID")
- # client_secret        = env("ARM_CLIENT_SECRET")
+    resource_group_name  = "actions"
+    storage_account_name = "tfstate1705"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    # tenant_id            = env("ARM_TENANT_ID")
+    #subscription_id      = env("ARM_SUBSCRIPTION_ID")
+    # client_id            = env("ARM_CLIENT_ID")
+    # client_secret        = env("ARM_CLIENT_SECRET")
   }
 }
